73d372e8ec166b5fa52c7bf4b2b4241ed10436d4
ttych <thomas.tych@gmail.com>
2019-02-21 13:49:55 +0100

    [build_tools] fix env for emacs

--- src/build_tools.sh
+++ src/build_tools.sh
@@ -59,16 +59,16 @@ BUILD_TOOLS_XBINDKEYS_DEFAULT_VERSION='1.8.6'
 #BUILD_TOOLS_EMACS_CONFIG='--with-x-toolkit=gtk2 --with-gif=no --with-x=yes'
 BUILD_TOOLS_EMACS_CONFIG='--with-x-toolkit=gtk3 --with-gif=no --with-x=yes --with-gnutls=yes'
 BUILD_TOOLS_EMACS_PREREQ_REDHAT='gtk2-devel libtiff-devel gnutls-devel'
-BUILD_TOOLS_EMACS_PREREQ_UBUNTU='libtiff5-dev libgif-dev libgnutls28-dev libgtk-3-dev libncurses-dev'
+BUILD_TOOLS_EMACS_PREREQ_UBUNTU='libtiff5-dev libgif-dev libgnutls28-dev libgtk-3-dev libncurses-dev libxpm-dev'
 BUILD_TOOLS_EMACS_URL_PATTERN='http://ftpmirror.gnu.org/emacs/emacs-${version}.tar.gz'
 BUILD_TOOLS_EMACS_DEFAULT_VERSION='26.1'
 
 #> emacs-nox - emacs editor without X
-BUILD_TOOLS_EMACS_CONFIG='--with-x=no --with-gif=no --with-gnutls=yes'
-BUILD_TOOLS_EMACS_PREREQ_REDHAT='libtiff-devel gnutls-devel'
-BUILD_TOOLS_EMACS_PREREQ_UBUNTU='libtiff5-dev libgif-dev libgnutls28-dev libncurses-dev'
-BUILD_TOOLS_EMACS_URL_PATTERN='http://ftpmirror.gnu.org/emacs/emacs-${version}.tar.gz'
-BUILD_TOOLS_EMACS_DEFAULT_VERSION='26.1'
+BUILD_TOOLS_EMACS_NOX_CONFIG='--with-x=no --with-gif=no --with-gnutls=yes'
+BUILD_TOOLS_EMACS_NOX_PREREQ_REDHAT='libtiff-devel gnutls-devel'
+BUILD_TOOLS_EMACS_NOX_PREREQ_UBUNTU='libtiff5-dev libgif-dev libgnutls28-dev libncurses-dev'
+BUILD_TOOLS_EMACS_NOX_URL_PATTERN='http://ftpmirror.gnu.org/emacs/emacs-${version}.tar.gz'
+BUILD_TOOLS_EMACS_NOX_DEFAULT_VERSION='26.1'
 
 #> vscode - Visual Studio Code
 BUILD_TOOLS_VSCODE_PREREQ_REDHAT='libXScrnSaver'
@@ -133,7 +133,7 @@ BUILD_TOOLS_TMUX_DEFAULT_VERSION='2.8'
 
 build_tools_env()
 {
-    build_tools_env__name="`echo ${1} | tr '[a-z]' '[A-Z]'`"
+    build_tools_env__name="`echo ${1} | tr '[a-z]-' '[A-Z]_'`"
 
     eval build_tools_env__url_pattern="\"\$BUILD_TOOLS_${build_tools_env__name}_URL_PATTERN\""
     [ -z "$build_tools_env__url_pattern" ] && return 1
